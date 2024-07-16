8858432bdaa9d5d5bc7479b3f7a816e46e29bde7
Nick <nickmufson1@gmail.com>
2024-07-16 14:59:16 -0400

    fix state logic / add shuffle

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -6,11 +6,18 @@ import { CardComponent } from './components/Card/Card';
 import { HeaderComponent } from './components/Header/Header';
 
 export function App() {
-  const arr = [];
-  for (let i = 1; i < 13; i++) {
-    arr.push(i * 3);
-  }
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      // destructing to reassign array elements
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 
+  const [arr, setArr] = useState([
+    3, 7, 11, 15, 20, 25, 30, 35, 40, 45, 50, 55,
+  ]);
   const [selectedIds, setSelectedIds] = useState([]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
@@ -22,17 +29,18 @@ export function App() {
     } else {
       setScore(score + 1);
       setSelectedIds([...selectedIds, pokemonId]);
-      setBestScore(Math.max(bestScore, score));
+      setBestScore(Math.max(bestScore, score + 1));
     }
+    setArr(shuffleArray(arr));
   };
 
   return (
     <>
       <HeaderComponent score={score} bestScore={bestScore}></HeaderComponent>
       <main>
-        {arr.map((value, index) => (
+        {arr.map((value) => (
           <CardComponent
-            key={index}
+            key={value}
             pokemonId={value}
             onClick={() => clickPokemon(value)}
           />
